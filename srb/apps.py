5570649bc60e0c471303db4e9e21e0cde8f323fa
Zafar Davlatov <97242244+davlatovv@users.noreply.github.com>
2023-02-26 02:16:40 +0500

    Add files via upload

---
+++ srb/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class SrbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'srb'
+    verbose_name = 'Social Rating Bot'
+
